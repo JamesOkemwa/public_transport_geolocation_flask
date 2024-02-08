c6c6f98589abb6fa84fb7ba346251b3770b560c1
lejames <okejames55@gmail.com>
2024-02-08 16:06:45 +0100

    Add user position on the map and center the map there

--- app.py
+++ app.py
@@ -5,29 +5,28 @@ from utility.helpers import read_gtfs_data
 
 app = Flask(__name__)
 
+# Create a folium map centered in Munster
+munster_coordinates = [51.96, 7.62]
+munster_map = folium.Map(location=munster_coordinates, tiles="cartodb positron", zoom_start=12)
+
+marker_cluster = MarkerCluster(
+    name='bus stops',
+    overlay=True,
+    control=False,
+    icon_create_function=None
+)
+
+# add bus stops markers to the map
+stops_df = read_gtfs_data()
+for index, bus_stop in stops_df.iterrows():
+    bus_stop_coords = [bus_stop['stop_lat'], bus_stop['stop_lon']]
+    marker = folium.Marker(location=bus_stop_coords, popup=bus_stop['stop_name'])
+    marker_cluster.add_child(marker)
+
+marker_cluster.add_to(munster_map)
+
 @app.route('/')
 def index():
-    # Create a folium map centered in Munster
-    munster_coordinates = [51.96, 7.62]
-    munster_map = folium.Map(location=munster_coordinates, tiles="cartodb positron", zoom_start=12)
-
-    marker_cluster = MarkerCluster(
-        name='bus stops',
-        overlay=True,
-        control=False,
-        icon_create_function=None
-    )
-
-    # add bus stops markers to the map
-    stops_df = read_gtfs_data()
-    for index, bus_stop in stops_df.iterrows():
-        bus_stop_coords = [bus_stop['stop_lat'], bus_stop['stop_lon']]
-        marker = folium.Marker(location=bus_stop_coords, popup=bus_stop['stop_name'])
-        marker_cluster.add_child(marker)
-
-    marker_cluster.add_to(munster_map)
-
-
     return render_template('map.html', folium_map=munster_map._repr_html_())
 
 @app.route('/update_user_location', methods=['POST'])
@@ -39,9 +38,14 @@ def update_user_location():
     latitude = data.get('latitude')
     longitude = data.get('longitude')
 
-    print("lat:", latitude, "long:",longitude)
+    # Add a marker for the user's location
+    user_location_marker = folium.Marker(location=[latitude, longitude], popup='You are here', icon=folium.Icon(color='red'))
+    user_location_marker.add_to(munster_map)
+    munster_map.position = [latitude, longitude]
 
-    return jsonify({'message': 'User location updated successfully'})
+    updated_map_html = munster_map._repr_html_()
+
+    return jsonify({'message': 'User location updated successfully', 'updated_map_html': updated_map_html})
 
 if __name__ == '__main__':
     app.run(debug=True)
