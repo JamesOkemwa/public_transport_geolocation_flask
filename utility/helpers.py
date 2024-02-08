46facf3298b000d813258fef78efcbda0fe85247
lejames <okejames55@gmail.com>
2024-02-08 17:34:29 +0100

    Add a helper function for geocoding place names and addresses

--- utility/helpers.py
+++ utility/helpers.py
@@ -1,4 +1,4 @@
-import geopandas as gpd
+from geopy.geocoders import Nominatim
 import pandas as pd
 
 def read_gtfs_data():
@@ -8,3 +8,16 @@ def read_gtfs_data():
     gtfs_file_path = 'data/gtfs/stadtwerke_feed/stops.txt'
     bus_stops_df = pd.read_csv(gtfs_file_path)
     return bus_stops_df
+
+def geocode_location(param):
+    """
+    Takes in a location name or address, geocodes it and returns the coordinates of the location.
+    """
+    geocoder = Nominatim(user_agent="Munster Public Transport")
+    location_coords = geocoder.geocode(param)
+
+    if location_coords:
+        latitude, longitude = location_coords.latitude, location_coords.longitude
+        return {'latitude': latitude, 'longitude': longitude}
+    else:
+        return None
